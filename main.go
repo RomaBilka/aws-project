13185bf7e583590c92021b7f444742e2972ece95
RomaBilka <roman_biliak@i.ua>
2022-12-09 20:08:32 +0200

    aws region

--- main.go
+++ main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "This is my website!\n")
+func getRegion(w http.ResponseWriter, r *http.Request) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Printf("error: %v", err)
+		return
+	}
+
+	client := imds.NewFromConfig(cfg)
+	region, err := client.GetRegion(context.TODO(), &imds.GetRegionInput{})
+	if err != nil {
+		log.Printf("Unable to retrieve the region from the EC2 instance %v\n", err)
+	}
+
+	fmt.Printf("region: %v\n", region)
+
+	res := struct {
+		Region string `json:"region"`
+	}{}
+	res.Region = region.Region
+	json.NewEncoder(w).Encode(res)
 }
 
 func main() {
-	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/", getRegion)
 	err := http.ListenAndServe(":80", nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
@@ -21,4 +44,5 @@ func main() {
 		fmt.Printf("error starting server: %s\n", err)
 		os.Exit(1)
 	}
+	fmt.Printf("run")
 }
